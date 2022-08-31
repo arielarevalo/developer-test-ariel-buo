86bf8ae4da7eed906a4169155d81b5c67bd9f0a3
Ariel Arévalo Alvarado <71473451+arielarevalo@users.noreply.github.com>
2022-08-31 16:04:18 -0600

    Initial commit.

---
+++ test_ariel/src/main/java/cr/co/arevalo/test_ariel/TestArielApplication.java
@@ -0,0 +1,13 @@
+package cr.co.arevalo.test_ariel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestArielApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestArielApplication.class, args);
+	}
+
+}
---
+++ test_ariel/src/test/java/cr/co/arevalo/test_ariel/TestArielApplicationTests.java
@@ -0,0 +1,13 @@
+package cr.co.arevalo.test_ariel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestArielApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
